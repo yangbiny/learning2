7d8acae99b666292125b1c25bfdec9ad6aae1fc8
tingfa.yang <tingfa.yang@duitang.com>
2023-11-24 15:53:04 +0800

    add es8 query

--- es8/src/main/java/com/reason/Main.java
+++ es8/src/main/java/com/reason/Main.java
@@ -1,13 +1,9 @@
 package com.reason;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.core.SearchRequest;
-import co.elastic.clients.elasticsearch.core.SearchRequest.Builder;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.SearchTemplateRequest;
 import co.elastic.clients.elasticsearch.core.SearchTemplateResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
-import co.elastic.clients.elasticsearch.core.search.SourceConfig;
 import co.elastic.clients.json.JsonData;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
@@ -18,16 +14,79 @@ import java.util.HashMap;
 import java.util.List;
 import lombok.Data;
 
+/*
+
+// 使用搜索模板进行查询
+GET sch-atlas-fine-20231120/_search/template
+{
+  "id": "my_search_template",
+  "params": {
+    "words":"壁纸",
+    "from":0,
+    "size":10,
+    "value":1000000000
+  }
+}
+
+// 渲染搜索模板（不执行查询）
+POST _render/template
+{
+  "id": "my_search_template",
+  "params": {
+    "words":"壁纸",
+    "from":1,
+    "size":1,
+    "value":100000
+  }
+}
+
+// 创建 搜索 模板
+PUT _scripts/my_search_template
+{
+  "script": {
+    "lang": "mustache",
+    "source": {
+      "query": {
+        "script_score": {
+          "script": {
+            "source": "_score + doc['time_latest_fav_at'].value / {{value}}"
+          },
+          "query": {
+            "bool": {
+              "must": [
+                {
+                  "match": {
+                    "msg": "{{words}}"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      },
+      "from": "{{from}}",
+      "size": "{{size}}"
+    }
+  }
+}
+
+
+
+ */
+
+
 public class Main {
 
   public static void main(String[] args) throws IOException {
     ElasticsearchClient client = ElasticSearchConfig.elasticsearchClient();
     var map = new HashMap<String, JsonData>();
 
-    map.put("words", JsonData.of("傻瓜"));
+    map.put("words", JsonData.of("好看的壁纸"));
     map.put("from", JsonData.of(1));
     map.put("size", JsonData.of(100));
+    map.put("value", JsonData.of(10000));
     SearchTemplateRequest request = new SearchTemplateRequest.Builder()
+        .index("sch-atlas-fine-20231120")
         .id("my_search_template")
         .params(map)
         .build();
