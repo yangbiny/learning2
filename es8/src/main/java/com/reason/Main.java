88bc3d527d0a56bcb328ed6b249f8dd7ca01ce78
tingfa.yang <tingfa.yang@duitang.com>
2023-11-28 15:46:29 +0800

    fix

--- es8/src/main/java/com/reason/Main.java
+++ es8/src/main/java/com/reason/Main.java
@@ -1,15 +1,25 @@
 package com.reason;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.FieldValue;
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchRequest.Builder;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.SearchTemplateRequest;
 import co.elastic.clients.elasticsearch.core.SearchTemplateResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.HitsMetadata;
 import co.elastic.clients.json.JsonData;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.reason.config.ElasticSearchConfig;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import lombok.Data;
@@ -77,14 +87,70 @@ PUT _scripts/my_search_template
 
 public class Main {
 
+  private static final JsonMapper mapper = new JsonMapper();
+
   public static void main(String[] args) throws IOException {
     ElasticsearchClient client = ElasticSearchConfig.elasticsearchClient();
+    //searchWithSearchAfter(client);
+    searchWithTemplate(client);
+  }
+
+  private static void searchWithSearchAfter(ElasticsearchClient client) throws IOException {
+
+    SortOptions timeCreateAt = SortOptions.of(
+        item -> item.field(f -> f.field("time_create_at")
+            .order(SortOrder.Desc)
+        )
+    );
+    SortOptions atlasId = SortOptions.of(
+        item -> item.field(f -> f.field("atlas_id").order(SortOrder.Desc)));
+
+    Query cntFav = Query.of(item -> item.term(t -> t.field("cnt_fav").value(0)));
+    Query createQuery = Query.of(item -> item.range(
+        r -> r.field("time_create_at").lt(JsonData.of(System.currentTimeMillis()))));
+
+    String searchAfter = "[1317886800000,264290]";
+    Object[] searchAfterObj = mapper.readValue(searchAfter, Object[].class);
+
+    List<FieldValue> searchAfterValue = new ArrayList<>();
+    for (Object o : searchAfterObj) {
+      searchAfterValue.add(FieldValue.of(Long.parseLong(o.toString())));
+    }
+
+    SearchRequest build = new Builder()
+        .query(q -> q.bool(bool -> bool.must(cntFav, createQuery)))
+        .sort(timeCreateAt, atlasId)
+        .size(10)
+        .searchAfter(searchAfterValue)
+        .build();
+    SearchResponse<SearchData> search = client.search(build, SearchData.class);
+    HitsMetadata<SearchData> hits = search.hits();
+    String after = "";
+    List<Hit<SearchData>> resultData = hits.hits();
+    if (!resultData.isEmpty()) {
+      Hit<SearchData> searchDataHit = resultData.get(resultData.size() - 1);
+      List<FieldValue> sort = searchDataHit.sort();
+      Object[] objects = new Object[sort.size()];
+      for (int i = 0; i < sort.size(); i++) {
+        objects[i] = sort.get(i)._get();
+      }
+      after = mapper.writeValueAsString(objects);
+    }
+    System.out.println(after);
+    System.out.println(hits);
+  }
+
+  private static void searchWithTemplate(ElasticsearchClient client) throws IOException {
     var map = new HashMap<String, JsonData>();
 
     map.put("words", JsonData.of("好看的壁纸"));
-    map.put("from", JsonData.of(1));
+    map.put("from", JsonData.of(0));
     map.put("size", JsonData.of(100));
     map.put("value", JsonData.of(10000));
+    var searchMap = new HashMap<>();
+    searchMap.put("first", 1310206301000L);
+    searchMap.put("second", 70289);
+    map.put("search_after_tpl", JsonData.of(searchMap));
     SearchTemplateRequest request = new SearchTemplateRequest.Builder()
         .index("sch-atlas-fine-20231120")
         .id("my_search_template")
@@ -96,6 +162,9 @@ public class Main {
 
     List<Hit<SearchData>> hits = response.hits().hits();
     List<SearchData> data = hits.stream().map(Hit::source).toList();
+    Hit<SearchData> searchDataHit = hits.get(hits.size() - 1);
+    List<FieldValue> sort = searchDataHit.sort();
+    System.out.println(sort);
     System.out.println(data);
   }
 }
